db165336190be8343787f745fc332ae703e59807
mpowers1113 <powers.matthew.douglas@gmail.com>
2021-11-10 01:11:24 +0000

    Completed first feature.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,30 @@
 /* exported data */
+
+var exercises = {};
+
+window.addEventListener('load', fetchExercises);
+
+
+function fetchExercises() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://wger.de/api/v2/exercise/?language=2&limit=300");
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function(){
+    console.log(xhr.status);
+    data = xhr.response.results;
+    exercises.biceps = data.filter(item => item.muscles.includes(1));
+    exercises.delts = data.filter(item => item.muscles.includes(2));
+    exercises.chest = data.filter(item => item.muscles.includes(4));
+    exercises.triceps = data.filter(item => item.muscles.includes(5));
+    exercises.abs = data.filter(item => item.muscles.includes(6));
+    exercises.glutes = data.filter(item => item.muscles.includes(8));
+    exercises.quads = data.filter(item => item.muscles.includes(10));
+    exercises.hams = data.filter(item => item.muscles.includes(11));
+    exercises.obliques = data.filter(item => item.muscles.includes(14));
+    exercises.traps = data.filter(item => item.muscles.includes(9));
+    exercises.lats = data.filter(item => item.muscles.includes(12));
+    console.log(data);
+    });
+  xhr.send()
+}
--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ var $legsButton = document.querySelector('#legs-button');
 var $dataViews = document.querySelectorAll('[data-view]');
 var $navItems = document.querySelectorAll('.nav');
 var $goBack = document.querySelectorAll('.back');
+var $deltsList = document.querySelector('.delts-list');
+var $chestList = document.querySelector('.chest-list');
+var $tricepsList = document.querySelector('.triceps-list');
+var $latsList = document.querySelector('.lats-list');
+var $trapsList = document.querySelector('.traps-list');
+var $bicepsList = document.querySelector('.biceps-list');
+var $quadsList = document.querySelector('.quads-list');
+var $glutesList = document.querySelector('.glutes-list');
+var $hamsList = document.querySelector('.hams-list');
 
+var $allUl = document.querySelectorAll('ul');
 
 // ----------Toggle New Entries ----------
 
@@ -41,3 +51,157 @@ function goBackHandler(){
 }
 
 $goBack.forEach(item => item.addEventListener('click', goBackHandler));
+
+//----------Render Push Exercises------------------
+
+function renderDelts() {
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.delts.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.delts[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.delts[i].name;
+    $li.appendChild($p)
+    $deltsList.appendChild($li);
+    numExercises++;
+      }
+    }      
+
+$pushButton.addEventListener('click', renderDelts);
+
+
+// ---------Render Chest -------------------
+
+function renderChest(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.chest.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.chest[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.chest[i].name;
+    $li.appendChild($p)
+    $chestList.appendChild($li);
+    numExercises++;
+      }
+}
+
+$pushButton.addEventListener('click', renderChest);
+
+// ----------Render Triceps-----------------
+
+function renderTriceps(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.triceps.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.triceps[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.triceps[i].name;
+    $li.appendChild($p)
+    $tricepsList.appendChild($li);
+    numExercises++;
+      }
+}
+$pushButton.addEventListener('click', renderTriceps)
+
+//---------Render Quads ----------------
+
+function renderQuads(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.quads.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.quads[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.quads[i].name;
+    $li.appendChild($p)
+    $quadsList.appendChild($li);
+    numExercises++;
+      }
+}
+
+$legsButton.addEventListener('click', renderQuads);
+
+//-----------Render Glutes---------------
+
+function renderGlutes(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.glutes.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.glutes[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.glutes[i].name;
+    $li.appendChild($p)
+    $glutesList.appendChild($li);
+    numExercises++;
+      }
+}
+
+$legsButton.addEventListener('click', renderGlutes);
+
+//-----------Render Hams------------------
+
+function renderHams(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.hams.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.hams[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.hams[i].name;
+    $li.appendChild($p)
+    $hamsList.appendChild($li);
+    numExercises++;
+      }
+}
+
+$legsButton.addEventListener('click', renderHams);
+
+//-----------Render Traps------------------
+
+function renderTraps(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.traps.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.traps[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.traps[i].name;
+    $li.appendChild($p)
+    $trapsList.appendChild($li);
+    numExercises++;
+      }
+}
+
+$pullButton.addEventListener('click', renderTraps);
+
+//-----------Render Lats-------------------
+
+function renderLats(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.lats.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.lats[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.lats[i].name;
+    $li.appendChild($p)
+    $latsList.appendChild($li);
+    numExercises++;
+      }
+}
+
+$pullButton.addEventListener('click', renderLats);
+
+//-----------Render Biceps------------------
+
+function renderBiceps(){
+  var numExercises = 0;
+  for (var i = Math.floor(Math.random() * exercises.biceps.length); numExercises< 2; i++){
+    var $li = document.createElement('li');
+    $li.setAttribute('id', exercises.biceps[i].uuid);
+    var $p = document.createElement('p');
+    $p.textContent = exercises.biceps[i].name;
+    $li.appendChild($p)
+    $bicepsList.appendChild($li);
+    numExercises++;
+      }
+}
+
+$pullButton.addEventListener('click', renderBiceps);
+
